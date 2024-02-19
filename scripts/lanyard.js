7a737c2701ab0f38c8d67eb52ab9c41ccb8c157a
Inter <me@inttter.com>
2024-02-19 18:28:11 +0000

    ✨ Add currently playing status

--- scripts/lanyard.js
+++ scripts/lanyard.js
@@ -2,29 +2,54 @@ function fetchPresenceData() {
     fetch('https://api.lanyard.rest/v1/users/514106760299151372')
         .then(response => response.json())
         .then(data => {
-            console.log('Received presence data:', data);
             if (data?.success) {
                 const discordStatus = data?.data?.discord_status;
+                let platform = '';
+                if (data?.data?.active_on_discord_desktop) {
+                    platform = 'Desktop';
+                } else if (data?.data?.active_on_discord_mobile) {
+                    platform = 'Mobile';
+                } else if (data?.data?.active_on_discord_web) {
+                    platform = 'Web';
+                }
                 let statusText = '';
                 switch (discordStatus) {
                     case 'online':
-                        statusText = 'Online 💻';
+                        statusText = `Online ${platform ? `on ${platform}` : ''}`;
                         break;
                     case 'idle':
-                        statusText = 'Idle ⌨️';
+                        statusText = `Idle ${platform ? `on ${platform}` : ''}`;
                         break;
                     case 'dnd':
-                        statusText = 'Do Not Disturb 🛏️';
+                        statusText = `Do Not Disturb ${platform ? `on ${platform}` : ''}`;
                         break;
                     default:
-                        statusText = 'Offline 🌙';
+                        statusText = 'Offline';
                         break;
                 }
+                
+                if (data?.data?.activities && data.data.activities.length > 0) {
+                    let spotifyPlaying = false;
+                    data.data.activities.forEach(activity => {
+                        if (activity.name === "Spotify" && data.data.listening_to_spotify) {
+                            spotifyPlaying = true;
+                        }
+                    });
+                    const nonSpotifyActivity = data.data.activities.find(activity => activity.name !== "Spotify");
+                
+                    if (nonSpotifyActivity?.name && nonSpotifyActivity?.details) {
+                        const activityTextElement = document.getElementById('activityText');
+                        activityTextElement.textContent = `Playing ${nonSpotifyActivity.name}`;
+                    } else if (spotifyPlaying) {
+                        const activityTextElement = document.getElementById('activityText');
+                        activityTextElement.textContent = '';
+                    }
+                }
+
                 const dotElement = document.getElementById('dot');
                 dotElement.className = `absolute bottom-1 right-1 tooltip tooltip-top tooltip-primary font-bold w-4 h-4 rounded-full ${discordStatus === 'offline' ? 'bg-gray-500' : (discordStatus === 'online' ? 'bg-green-500' : (discordStatus === 'idle' ? 'bg-yellow-500' : 'bg-red-500'))}`;
                 dotElement.setAttribute('data-tip', statusText);
 
-                // Update the listeningTo element with the Spotify information
                 const listeningToElement = document.getElementById('listeningTo');
                 if (data?.data?.listening_to_spotify) {
                     const spotifyData = data.data.spotify;
@@ -43,8 +68,6 @@ function fetchPresenceData() {
         });
 }
 
-// Fetch presence data when the page loads
 window.addEventListener('load', fetchPresenceData);
 
-// Fetch presence data every 60 seconds
 setInterval(fetchPresenceData, 60000);
