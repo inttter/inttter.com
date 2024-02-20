83433e185a8611c0ae5e12343d1692cd9505fef7
Inter <me@inttter.com>
2024-02-20 22:57:03 +0000

    🧹 Make getting to the video easier

---
+++ scripts/openvideo.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() { 
+    const profilePicture = document.querySelector('.profile-picture');
+
+    profilePicture.addEventListener('click', function() {
+        window.open('https://inttter.com/assets/video/well-done.mp4');
+    });
+});
